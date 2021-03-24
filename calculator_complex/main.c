f9cd2a2b2376e31377c88277b89784c538cb656b
Jay Singh <81297719+codemonk-007@users.noreply.github.com>
2021-03-24 23:31:13 +0530

    Update main.c

--- calculator_complex/main.c
+++ calculator_complex/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "complex_calculator.h"
 /**
- * @brief The main functions runs the complex calculator program on 5 different inputs and outputs
+ * @brief The main functions runs the complex calculator program on 5 different inputs and outputs the results of all the 4 operations performed
  * 
  * @return int 
  */
@@ -44,3 +44,37 @@ int main()
     } 
     return 0;
 }
+/* OUTPUT
+Complex calculator :
+
+Performing all operations on numbers 5.000000 + (2.400000)i  and 10.000000 + (-1.450000)i
+Sum = 15.000000 + (0.950000) i
+Difference = -5.000000 + (3.850000) i
+Product = 53.480000 + (16.750000) i
+Quotient = 0.455621 + (0.306065) i
+
+Performing all operations on numbers 23.000000 + (3.600000)i  and 17.000000 + (-2.340000)i
+Sum = 40.000000 + (1.260000) i
+Difference = 6.000000 + (5.940000) i
+Product = 399.424011 + (7.379997) i
+Quotient = 1.299177 + (0.390593) i
+
+Performing all operations on numbers 4.000000 + (-2.000000)i  and -9.000000 + (9.870000)i
+Sum = -5.000000 + (7.870000) i
+Difference = 13.000000 + (-11.870000) i
+Product = -16.260000 + (57.480000) i
+Quotient = -0.312414 + (-0.120392) i
+
+Performing all operations on numbers -7.000000 + (1.500000)i  and -5.000000 + (0.560000)i
+Sum = -12.000000 + (2.060000) i
+Difference = -2.000000 + (0.940000) i
+Product = 34.160000 + (-11.420000) i
+Quotient = 1.415840 + (-0.141426) i
+
+Performing all operations on numbers 0.000000 + (-11.000000)i  and 4.000000 + (-14.140000)i
+Sum = 4.000000 + (-25.139999) i
+Difference = -4.000000 + (3.140000) i
+Product = -155.540009 + (-44.000000) i
+Quotient = 0.720294 + (-0.203761) i
+
+*/
